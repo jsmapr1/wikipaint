8285ebd2d81504b9060953b876d34f0c8074e8ad
jsmapr1 <jsmapr1@gmail.com>
2013-07-27 19:41:10 -0500

    Set up js lint

--- jakefile.js
+++ jakefile.js
@@ -1,4 +1,42 @@
-desc('example');
-task('example', function(){
-  console.log('example task');
-});
+/* global desc, task, jake, fail, complete */
+(function() {
+    "use strict";
+
+    task('default', ['lint']);
+
+    desc('lint everything');
+    task('lint', [], function() {
+        var lint = require('./build/lint/lint_runner.js'),
+        files = new jake.FileList();
+        files.include('**/*.js');
+        files.exclude('node_modules');
+        files.exclude('build');
+
+        lint.validateFileList(files.toArray(), nodeLintOptions(), {});
+    });
+
+    desc('example');
+    task('example', function() {
+      console.log('example task');
+    });
+
+    function nodeLintOptions() {
+        return {
+            bitwise: true,
+            curly: false,
+            eqeqeq: true,
+            forin: true,
+            immed: true,
+            latedef: true,
+            newcap: true,
+            noarg: true,
+            noempty: true,
+            nonew: true,
+            regexp: true,
+            undef: true,
+            strict: true,
+            trailing: true,
+            node: true
+        };
+    }
+}());
